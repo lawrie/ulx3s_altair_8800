10c351f24f9adc0f5ab3ebd7d0398c7eba427eb0
lawrie <lawrie.griffiths@ntlword.com>
2021-04-25 10:47:41 +0100

    Start of OSD

--- src/top/top_altair.v
+++ src/top/top_altair.v
@@ -16,6 +16,11 @@ module top
   input [3:0]   sw,
   // GPIO
   inout  [27:0] gp,gn,
+  // SPI from ESP32
+  input         wifi_gpio16,
+  input         wifi_gpio5,
+  output        wifi_gpio0,
+  inout   [3:0] sd_d,  
   // HDMI
   output [3:0]  gpdi_dp,
   output [3:0]  gpdi_dn,
@@ -40,6 +45,7 @@ module top
   );
 
   wire clk_vga = clocks[1];
+  wire clk_cpu = clocks[1];
   wire clk_hdmi = clocks[0];
 
   // ===============================================================
@@ -74,12 +80,13 @@ module top
   reg [7:0]   dir = 0; // Dummy to make tri-state work
   reg         protect = 0, inte = 0, hlda = 0, wt = ~sw[0];
 
+  reg [6:0]   r_btn_joy;
+  wire [10:0] ps2_key;
+
   // ===============================================================
   // Built-in leds
   // ===============================================================
-  wire [10:0] ps2_key;
-
-  assign led = statuLEDs;
+  assign led = statusLEDs;
 
   // ===============================================================
   // GPIO pins
@@ -103,7 +110,7 @@ module top
   // Altair 8800
   // ===============================================================
   altair machine(
-    .clk(clk_vga),
+    .clk(clk_cpu),
     .reset(~resetn),
     .rx(ftdi_txd),
     .tx(ftdi_rxd),
@@ -151,20 +158,79 @@ module top
   );
 
   // ===============================================================
+  // SPI Slave from ESP32
+  // ===============================================================
+  wire        spi_ram_wr, spi_ram_rd;
+  wire [31:0] spi_ram_addr;
+  wire [7:0]  spi_ram_di;
+  wire [7:0]  ram_out;
+  wire [7:0]  spi_ram_do = ram_out;
+  wire        irq;
+
+  assign sd_d[3] = 1'bz; // FPGA pin pullup sets SD card inactive at SPI bus
+  assign wifi_gpio0 = ~irq;
+
+  spi_ram_btn #(
+    .c_sclk_capable_pin(1'b0),
+    .c_addr_bits(32)
+  ) spi_ram_btn_inst (
+    .clk(clk_cpu),
+    .csn(~wifi_gpio5),
+    .sclk(wifi_gpio16),
+    .mosi(sd_d[1]), // wifi_gpio4
+    .miso(sd_d[2]), // wifi_gpio12
+    .btn(r_btn_joy),
+    .irq(irq),
+    .wr(spi_ram_wr),
+    .rd(spi_ram_rd),
+    .addr(spi_ram_addr),
+    .data_in(spi_ram_do),
+    .data_out(spi_ram_di)
+  );
+
+  // ===============================================================
+  // OSD
+  // ===============================================================
+  wire [7:0] osd_vga_r, osd_vga_g, osd_vga_b;
+  wire osd_vga_hsync, osd_vga_vsync, osd_vga_blank;
+
+  spi_osd #(
+    .c_start_x(62), .c_start_y(80),
+    .c_chars_x(64), .c_chars_y(20),
+    .c_init_on(0),
+    .c_char_file("osd.mem"),
+    .c_font_file("font_bizcat8x16.mem")
+  ) spi_osd_inst (
+    .clk_pixel(clk_vga), .clk_pixel_ena(1),
+    .i_r(red),
+    .i_g(green),
+    .i_b(blue),
+    .i_hsync(~hsync), .i_vsync(~vsync), .i_blank(~vga_de),
+    .i_csn(~wifi_gpio5), .i_sclk(wifi_gpio16), .i_mosi(sd_d[1]), // .o_miso(),
+    .o_r(osd_vga_r), .o_g(osd_vga_g), .o_b(osd_vga_b),
+    .o_hsync(osd_vga_hsync), .o_vsync(osd_vga_vsync), .o_blank(osd_vga_blank)
+  );
+
+  // ===============================================================
   // Convert VGA to HDMI
   // ===============================================================
   HDMI_out vga2dvid (
     .pixclk(clk_vga),
     .pixclk_x5(clk_hdmi),
-    .red(red),
-    .green(green),
-    .blue(blue),
-    .vde(vga_de),
-    .hSync(hsync),
-    .vSync(vsync),
+    .red(osd_vga_r),
+    .green(osd_vga_g),
+    .blue(osd_vga_b),
+    .vde(~osd_vga_blank),
+    .hSync(osd_vga_hsync),
+    .vSync(osd_vga_vsync),
     .gpdi_dp(gpdi_dp),
     .gpdi_dn(gpdi_dn)
   );
+
+  // ===============================================================
+  // Joystick for OSD control and games
+  // ===============================================================
+  always @(posedge clk_cpu) r_btn_joy <= btn;
 
   // ===============================================================
   // Keyboard
@@ -174,7 +240,7 @@ module top
 
   // Get PS/2 keyboard events
   ps2 ps2_kbd (
-    .clk(clk_vga),
+    .clk(clk_cpu),
     .ps2_clk(usb_fpga_bd_dp),
     .ps2_data(usb_fpga_bd_dn),
     .ps2_key(ps2_key)
